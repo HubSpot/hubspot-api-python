16a6dc671fe232d73122dcba094a4c32535e9513
Aleh Tanasiuk <atanasiuk@hubspot.com>
2020-02-07 13:56:23 +0530

    Clean up

--- hubspot/discovery/auth/oauth/discovery.py
+++ hubspot/discovery/auth/oauth/discovery.py
@@ -5,7 +5,6 @@ import hubspot.codegen.auth.oauth.exceptions as exceptions
 class Discovery:
     def default_api(self):
         configuration = oauth.Configuration()
-        configuration.api_key['hapikey'] = '220a09eb-7f6c-4012-99f7-7c7f0692f18a'
 
         return oauth.DefaultApi(oauth.ApiClient(configuration))
 
