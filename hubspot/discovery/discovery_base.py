1b4ac56d60a045e5cd922fc6a6e154196a581ffb
Aleh Tanasiuk <atanasiuk@hubspot.com>
2020-02-10 13:29:57 +0530

    Fix configuring of api_key

--- hubspot/discovery/discovery_base.py
+++ hubspot/discovery/discovery_base.py
@@ -2,9 +2,11 @@ class DiscoveryBase:
     def __init__(self, config):
         self.config = config
 
-    def _configure_api_client(self, client_module, api_name):
-        configuration = client_module.Configuration()
-        configuration.api_key = self.config["api_key"]
-        configuration.access_token = self.config["access_token"]
+    def _configure_api_client(self, api_client, api_name):
+        configuration = api_client.Configuration()
+        if self.config["api_key"] is not None:
+            configuration.api_key['hapikey'] = self.config["api_key"]
+        if self.config["access_token"] is not None:
+            configuration.access_token = self.config["access_token"]
 
-        return getattr(client_module, api_name)(client_module.ApiClient(configuration))
+        return getattr(api_client, api_name)(api_client.ApiClient(configuration))
