66fd904be0cd223eb7329cc5a7eed3ad1d37da57
Aleh Tanasiuk <atanasiuk@hubspot.com>
2020-02-12 13:48:02 +0530

    Add OAuth login page

--- sample-apps/oauth-app/src/app.py
+++ sample-apps/oauth-app/src/app.py
@@ -1,11 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
+
+import routes
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def hello():
-    return 'This is OAuth sample app'
+app.register_blueprint(routes.oauth)
 
 
 if __name__ == "__main__":
---
+++ sample-apps/oauth-app/src/routes/__init__.py
@@ -0,0 +1,1 @@
+from .oauth import module as oauth
