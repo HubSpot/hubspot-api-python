6d5c3eb6ac39261982a9f6e3ae1d07d53dea12bd
yfaktorovich <yfaktorovich@hubspot.com>
2020-02-21 18:51:22 -0500

    added companies

--- hubspot/discovery/crm/discovery.py
+++ hubspot/discovery/crm/discovery.py
@@ -1,7 +1,11 @@
 from ..discovery_base import DiscoveryBase
 from .contacts.discovery import Discovery as ContactsDiscovery
+from .companies.discovery import Discovery as CompaniesDiscovery
 
 
 class Discovery(DiscoveryBase):
     def contacts(self):
         return ContactsDiscovery(self.config)
+
+    def companies(self):
+        return CompaniesDiscovery(self.config)
