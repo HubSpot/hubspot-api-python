b6d41d44b68768c74a511820e19e78c77ad1ae03
Aleh Tanasiuk <atanasiuk@hubspot.com>
2020-03-05 15:57:36 +0300

    Clean up

--- hubspot/utils/objects.py
+++ hubspot/utils/objects.py
@@ -1,9 +1,12 @@
+PAGE_MAX_SIZE = 100
+
 
 def fetch_all(get_page_api_client, **kwargs):
     results = []
-    after = 0
+    after = None
+
     while True:
-        page = get_page_api_client.get_page(after=after, limit=100, **kwargs)
+        page = get_page_api_client.get_page(after=after, limit=PAGE_MAX_SIZE, **kwargs)
         results.extend(page.results)
         if page.paging is None:
             break
