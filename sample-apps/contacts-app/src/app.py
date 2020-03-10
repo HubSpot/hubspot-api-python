d371e73953996a090a3e1b080e334d488ac99601
Aleh Tanasiuk <atanasiuk@hubspot.com>
2020-03-10 14:42:45 +0300

    Add contacts app init structure

---
+++ sample-apps/contacts-app/src/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, redirect
+
+import routes
+
+app = Flask(__name__)
+
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+app.register_blueprint(routes.oauth, url_prefix='/oauth')
+app.register_blueprint(routes.contacts, url_prefix='/contacts')
+
+
+@app.route('/')
+def contacts():
+    return redirect('/contacts')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
---
+++ sample-apps/contacts-app/src/routes/contacts.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, render_template
+from helpers.hubspot import create_client
+from auth import oauth_required
+
+
+module = Blueprint(__name__, __name__)
+
+
+@module.route('/')
+@oauth_required
+def contacts():
+    hubspot = create_client()
+    contacts_page = hubspot.crm().contacts().basic_api().get_page()
+
+    return render_template('contacts/list.html', contacts=contacts_page.results)
