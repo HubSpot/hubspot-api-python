a409945f52e65042fa7cdf11015380c0b2a0c61c
Aleh Tanasiuk <atanasiuk@hubspot.com>
2020-03-12 15:50:56 +0300

    Add show/edit of contact

--- sample-apps/contacts-app/src/routes/contacts.py
+++ sample-apps/contacts-app/src/routes/contacts.py
@@ -1,18 +1,46 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect
 from helpers.hubspot import create_client
 from auth import oauth_required
-from hubspot.crm.contacts import PublicObjectSearchRequest, Filter, FilterGroup
+from pprint import pprint
+from hubspot.crm.contacts import PublicObjectSearchRequest, SimplePublicObject, Filter, FilterGroup
 
 module = Blueprint(__name__, __name__)
 
 
 @module.route('/')
 @oauth_required
-def contacts():
+def list():
     hubspot = create_client()
     contacts_page = hubspot.crm().contacts().basic_api().get_page()
 
     return render_template('contacts/list.html', contacts=contacts_page.results)
+
+
+@module.route('/<contact_id>')
+@oauth_required
+def show(contact_id):
+    pprint(__name__)
+    hubspot = create_client()
+    all_properties = hubspot.crm().properties().core_api().get_all('CONTACTS')
+    editable_properties = []
+    for p in all_properties.results:
+        if p.type == 'string' and p.modification_metadata.read_only_value is False:
+            editable_properties.append(p)
+    contact = hubspot.crm().contacts().basic_api().get_by_id(
+        contact_id,
+        properties=[p.name for p in editable_properties],
+    )
+    editable_properties_dict = {p.name: p for p in editable_properties}
+    return render_template('contacts/show.html', contact=contact, properties_dict=editable_properties_dict)
+
+
+@module.route('/<contact_id>', methods=['POST'])
+@oauth_required
+def update(contact_id):
+    properties = SimplePublicObject(properties=request.form)
+    hubspot = create_client()
+    hubspot.crm().contacts().basic_api().update(contact_id, simple_public_object_input=properties)
+    return redirect(request.url, code=302)
 
 
 @module.route('/search')
