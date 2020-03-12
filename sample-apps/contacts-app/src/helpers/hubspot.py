d73225e7aaeddaf0bc17bfed008e5c6d3ca7aeec
Aleh Tanasiuk <atanasiuk@hubspot.com>
2020-03-12 15:54:27 +0300

    Fix create client

--- sample-apps/contacts-app/src/helpers/hubspot.py
+++ sample-apps/contacts-app/src/helpers/hubspot.py
@@ -5,6 +5,8 @@ from .oauth import refresh_and_get_access_token, is_authenticated
 
 def create_client():
     api_key = os.environ.get('HUBSPOT_API_KEY')
+    if is_authenticated():
+        return hubspot.Client.create(access_token=refresh_and_get_access_token())
     if api_key is not None:
         return hubspot.Client.create(api_key=api_key)
 
