bd8250c70f393e084b5b2ea5f11e874116d13806
Aleh Tanasiuk <atanasiuk@hubspot.com>
2020-03-13 15:03:07 +0300

    Add properties list/show actions

--- sample-apps/contacts-app/src/app.py
+++ sample-apps/contacts-app/src/app.py
@@ -8,6 +8,7 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 app.register_blueprint(routes.oauth, url_prefix='/oauth')
 app.register_blueprint(routes.contacts, url_prefix='/contacts')
+app.register_blueprint(routes.properties, url_prefix='/properties')
 
 
 @app.route('/')
