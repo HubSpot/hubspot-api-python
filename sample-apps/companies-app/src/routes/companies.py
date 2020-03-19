82fd03c4c7176d95aa78009d2868c5e745fe0fcc
Aleh Tanasiuk <atanasiuk@hubspot.com>
2020-03-19 11:43:17 +0300

    Add new company action

--- sample-apps/companies-app/src/routes/companies.py
+++ sample-apps/companies-app/src/routes/companies.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, request
-from hubspot.crm.companies import PublicObjectSearchRequest, Filter, FilterGroup
+from flask import Blueprint, render_template, request, redirect, url_for
+from hubspot.crm.companies import PublicObjectSearchRequest, Filter, FilterGroup, SimplePublicObject, SimplePublicObjectInput
 from helpers.hubspot import create_client
 from auth import auth_required
 
@@ -18,6 +18,27 @@ def list():
     companies_page = hubspot.crm().companies().search_api().do_search(public_object_search_request=search_request)
 
     return render_template('companies/list.html', companies=companies_page.results)
+
+
+@module.route('/new')
+@auth_required
+def new():
+    return render_template('companies/show.html', company=SimplePublicObject())
+
+
+@module.route('/new', methods=['POST'])
+@auth_required
+def create():
+    properties = SimplePublicObjectInput(properties=request.form)
+    hubspot = create_client()
+    company = hubspot.crm().companies().basic_api().create(simple_public_object_input=properties)
+    return redirect(url_for('companies.show', company_id=company.id))
+
+
+@module.route('/<company_id>')
+@auth_required
+def show(company_id):
+    return redirect(url_for('companies.list'))
 
 
 @module.route('/search')
