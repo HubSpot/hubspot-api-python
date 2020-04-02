6c600b2286efd9da798579bb8fcf63616c3db9f2
Aleh Tanasiuk <atanasiuk@hubspot.com>
2020-04-02 11:49:22 +0300

    Add webhooks client

---
+++ hubspot/webhooks/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from hubspot.webhooks.api.settings_api import SettingsApi
+from hubspot.webhooks.api.subscriptions_api import SubscriptionsApi
