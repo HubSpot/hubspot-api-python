57cf236412368ac075014931bdb706af33cb90b8
Aleh Tanasiuk <atanasiuk@hubspot.com>
2020-04-10 11:00:42 +0300

    Add sdk client type hinting

--- hubspot/client.py
+++ hubspot/client.py
@@ -1,14 +1,15 @@
+from urllib3.util.retry import Retry
 from .discovery.discovery import Discovery
 
 
 class Client:
     @staticmethod
     def create(
-        api_key=None,
-        access_token=None,
-        retries=None,
+        api_key: str = None,
+        access_token: str = None,
+        retries: Retry = None,
         **kwargs
-    ):
+    ) -> Discovery:
         kwargs['api_key'] = api_key
         kwargs['access_token'] = access_token
         kwargs['retries'] = retries
