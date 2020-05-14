a32778e052224f06ae26c6d869b7b4f79bac6963
ksvirkou <ksvirkou@hubspot.com>
2020-05-14 22:32:36 +0300

    rewrite worker

--- sample-apps/retry-middleware-app/src/helpers/hubspot.py
+++ sample-apps/retry-middleware-app/src/helpers/hubspot.py
@@ -5,6 +5,11 @@ from urllib3.util.retry import Retry
 
 def create_client():
     if is_authenticated():
-        return hubspot.Client.create(access_token=refresh_and_get_access_token())
+        retry = Retry(
+            total=3,
+            backoff_factor=5,
+            status_forcelist=(429, 500, 502, 504),
+        )
+        return hubspot.Client.create(access_token=refresh_and_get_access_token(), retry = retry)
 
     return hubspot.Client.create()
