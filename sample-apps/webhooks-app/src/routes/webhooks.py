f9f259909c75a6f13a5cfffc1e539aa990616dde
Aleh Tanasiuk <atanasiuk@hubspot.com>
2020-05-18 14:45:20 +0300

    Add kafka & producer

--- sample-apps/webhooks-app/src/routes/webhooks.py
+++ sample-apps/webhooks-app/src/routes/webhooks.py
@@ -1,11 +1,18 @@
-from flask import Blueprint
-from auth import auth_required
+from flask import Blueprint, request
+import json
+import os
+from services.kafka import get_producer
 
 
 module = Blueprint("webhooks", __name__)
 
 
 @module.route("/handle", methods=["POST"])
-@auth_required
 def handle():
-    pass
+    data = request.data
+    events = json.loads(data)
+    producer = get_producer()
+    for event in events:
+        producer.send(os.getenv("EVENTS_TOPIC"), value=event)
+
+    return "", 204
