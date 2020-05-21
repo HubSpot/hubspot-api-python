bb3b3a766b6eb2343ee45c48946b02bc06189121
Aleh Tanasiuk <atanasiuk@hubspot.com>
2020-05-21 13:51:45 +0300

    Add verify signature util

---
+++ hubspot/exceptions.py
@@ -0,0 +1,9 @@
+
+class InvalidSignatureError(Exception):
+    def __init__(self, msg=None, signature=None, signature_version=None, hash_result=None):
+        self.signature = signature
+        self.signature_version = signature_version
+        self.hash_result = hash_result
+        if msg is None:
+            msg = "Invalid signature passed to request"
+        super(InvalidSignatureError, self).__init__(msg)
