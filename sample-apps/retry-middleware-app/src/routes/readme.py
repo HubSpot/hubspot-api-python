724e6cc6f4f7dd3f2ac8ec5109273f11ff5601af
ksvirkou <ksvirkou@hubspot.com>
2020-05-22 10:36:37 +0300

    bf

--- sample-apps/retry-middleware-app/src/routes/readme.py
+++ sample-apps/retry-middleware-app/src/routes/readme.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, render_template
 from auth import auth_required
-from helpers.hubspot import create_client
 
 
 module = Blueprint("readme", __name__)
@@ -9,6 +8,4 @@ module = Blueprint("readme", __name__)
 @module.route("/")
 @auth_required
 def readme():
-    hubspot = create_client()
-    hubspot.crm.contacts.basic_api.get_page()
     return render_template("readme/readme.html")
