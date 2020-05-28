da124781f024cb1b55e4e8b36c80d0a75073c9e5
Aleh Tanasiuk <atanasiuk@hubspot.com>
2020-05-28 13:29:04 +0300

    Add imports readme page

--- sample-apps/imports-app/src/routes/imports.py
+++ sample-apps/imports-app/src/routes/imports.py
@@ -1,5 +1,10 @@
-from flask import Blueprint, render_template
+import json
+import tempfile
+import os
+from flask import Blueprint, render_template, request
+from werkzeug.utils import secure_filename
 from auth import auth_required
+from helpers.hubspot import create_client
 
 
 module = Blueprint("imports", __name__)
@@ -9,3 +14,43 @@ module = Blueprint("imports", __name__)
 @auth_required
 def readme():
     return render_template("imports/readme.html")
+
+
+@module.route("/start", methods=["POST"])
+@auth_required
+def start():
+    file = request.files["file"]
+
+    handle, filepath = tempfile.mkstemp(suffix=".csv")
+    os.close(handle)
+    file.save(filepath)
+
+    import_request = {
+        "name": "Import '{}'".format(file.filename),
+        "files": [
+            {
+                "fileName": os.path.basename(filepath),
+                "fileImportPage": {
+                    "hasHeader": True,
+                    "columnMappings": [
+                        {
+                            "columnName": "First Name",
+                            "propertyName": "firstname",
+                            "columnObjectType": "CONTACT",
+                        },
+                        {
+                            "columnName": "Email",
+                            "propertyName": "email",
+                            "columnObjectType": "CONTACT",
+                        },
+                    ],
+                },
+            }
+        ]
+    }
+    hubspot = create_client()
+    response = hubspot.crm.imports.core_api.create(files=filepath, import_request=json.dumps(import_request))
+
+    os.unlink(filepath)
+    return render_template("imports/result.html", response=response)
+
