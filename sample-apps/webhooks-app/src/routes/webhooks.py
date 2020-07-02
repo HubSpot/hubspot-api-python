24d7b8df97f363931b3760e6e364386b0ca80455
Aleh Tanasiuk <atanasiuk@hubspot.com>
2020-07-02 10:55:32 +0300

    Clean up webhooks sample app

--- sample-apps/webhooks-app/src/routes/webhooks.py
+++ sample-apps/webhooks-app/src/routes/webhooks.py
@@ -2,26 +2,15 @@ from flask import Blueprint, request
 import json
 import os
 from services.kafka import get_producer
-from hubspot.utils.webhooks import validate_signature
-from hubspot.exceptions import InvalidSignatureError
+from auth import hubspot_signature_required
 
 
 module = Blueprint("webhooks", __name__)
 
 
 @module.route("/handle", methods=["POST"])
+@hubspot_signature_required
 def handle():
-    try:
-        validate_signature(
-            signature=request.headers["X-HubSpot-Signature"],
-            signature_version=request.headers["X-HubSpot-Signature-Version"],
-            http_uri=request.base_url,
-            request_body=request.data.decode("utf-8"),
-            client_secret=os.getenv("HUBSPOT_CLIENT_SECRET"),
-        )
-    except InvalidSignatureError:
-        return "", 403
-
     events = json.loads(request.data)
     producer = get_producer()
     for event in events:
