e809153b157740ca7864ed0050be32bf5a773414
Aleh Tanasiuk <atanasiuk@hubspot.com>
2020-07-17 12:03:43 +0300

    Ad mappings repo

---
+++ sample-apps/trello-integration-app/src/repositories/mappings.py
@@ -0,0 +1,26 @@
+from services.db import session, Mapping, transactional
+
+
+class MappingsRepository:
+    @classmethod
+    @transactional
+    def save(cls, mapping):
+        session.add(mapping)
+        return mapping
+
+    @classmethod
+    @transactional
+    def find_by(cls, **kwargs):
+        return session.query(Mapping).filter_by(
+            **kwargs
+        ).all()
+
+    @classmethod
+    @transactional
+    def get(cls, mapping_id):
+        return session.query(Mapping).get(mapping_id)
+
+    @classmethod
+    @transactional
+    def delete_by_id(cls, mapping_id):
+        session.query(Mapping).filter_by(id=mapping_id).delete()
