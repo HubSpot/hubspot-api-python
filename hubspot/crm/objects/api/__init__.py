b32844041f804667aa6f897f7a03a991b08481c4
Aleh Tanasiuk <atanasiuk@hubspot.com>
2020-09-30 16:09:49 +0300

    Add objects&schemas clients (codegen only)

---
+++ hubspot/crm/objects/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from hubspot.crm.objects.api.associations_api import AssociationsApi
+from hubspot.crm.objects.api.basic_api import BasicApi
+from hubspot.crm.objects.api.batch_api import BatchApi
+from hubspot.crm.objects.api.search_api import SearchApi
---
+++ hubspot/crm/schemas/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from hubspot.crm.schemas.api.core_api import CoreApi
+from hubspot.crm.schemas.api.default_api import DefaultApi
