458f3a9bf49fd571d4338ac1a42bf57010d22645
Павел Лавринович <plaurynovich@hubspot.com>
2021-02-08 16:16:33 +0300

    Files and marketing tests + discovery

---
+++ hubspot/discovery/files/files/discovery.py
@@ -0,0 +1,12 @@
+import hubspot.files.files as api_client
+from ...discovery_base import DiscoveryBase
+
+
+class Discovery(DiscoveryBase):
+    @property
+    def files_api(self) -> api_client.FilesApi:
+        return self._configure_api_client(api_client, "FilesApi")
+
+    @property
+    def folders_api(self) -> api_client.FoldersApi:
+        return self._configure_api_client(api_client, "FoldersApi")
