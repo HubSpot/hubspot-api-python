784a96122910af345678ce6880d823be11340f21
Eric Leblond <el@stamus-networks.com>
2023-02-06 20:59:56 +0100

    util: allow limit to be set in get_all functions
    Reference: https://github.com/HubSpot/hubspot-api-python/issues/143

--- hubspot/utils/objects.py
+++ hubspot/utils/objects.py
@@ -6,7 +6,10 @@ def fetch_all(get_page_api_client, **kwargs):
     after = None
 
     while True:
-        page = get_page_api_client.get_page(after=after, limit=PAGE_MAX_SIZE, **kwargs)
+        if 'limit' in kwargs:
+            page = get_page_api_client.get_page(after=after, **kwargs)
+        else:
+            page = get_page_api_client.get_page(after=after, limit=PAGE_MAX_SIZE, **kwargs)
         results.extend(page.results)
         if page.paging is None:
             break
