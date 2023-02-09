d72d1a4f69b15b765215e57b7271dcd017ba4b2b
alexey <azheltkovskiy@hubspot.com>
2023-02-09 12:16:26 +0300

    add crm.associations.types_api into discovery

--- tests/spec/crm/test_associations.py
+++ tests/spec/crm/test_associations.py
@@ -1,7 +1,8 @@
 from hubspot import HubSpot
-from hubspot.crm.associations import BatchApi
+from hubspot.crm.associations import BatchApi, TypesApi
 
 
 def test_is_discoverable():
     apis = HubSpot().crm.associations
     assert isinstance(apis.batch_api, BatchApi)
+    assert isinstance(apis.types_api, TypesApi)
