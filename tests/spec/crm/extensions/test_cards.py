ad4cf660cfaab52e50992f9506f048199e2ac7ac
alexey <azheltkovskiy@hubspot.com>
2023-02-13 11:30:33 +0300

    add (crm.imports.public_imports_api, crm.extensions.cards.sample_response_api) to discovery

--- tests/spec/crm/extensions/test_cards.py
+++ tests/spec/crm/extensions/test_cards.py
@@ -1,7 +1,8 @@
 from hubspot import HubSpot
-from hubspot.crm.extensions.cards import CardsApi
+from hubspot.crm.extensions.cards import CardsApi, SampleResponseApi
 
 
 def test_is_discoverable():
     apis = HubSpot().crm.extensions.cards
     assert isinstance(apis.cards_api, CardsApi)
+    assert isinstance(apis.sample_response_api, SampleResponseApi)
--- tests/spec/crm/test_imports.py
+++ tests/spec/crm/test_imports.py
@@ -1,7 +1,8 @@
 from hubspot import HubSpot
-from hubspot.crm.imports import CoreApi
+from hubspot.crm.imports import CoreApi, PublicImportsApi
 
 
 def test_is_discoverable():
     apis = HubSpot().crm.imports
     assert isinstance(apis.core_api, CoreApi)
+    assert isinstance(apis.public_imports_api, PublicImportsApi)
