f8d34a11221b9247e43f93a4b68cad0e851c098b
alexey <azheltkovskiy@hubspot.com>
2023-02-20 15:37:48 +0300

    add test for associations v4

--- tests/spec/crm/test_associations.py
+++ tests/spec/crm/test_associations.py
@@ -6,3 +6,4 @@ def test_is_discoverable():
     apis = HubSpot().crm.associations
     assert isinstance(apis.batch_api, BatchApi)
     assert isinstance(apis.types_api, TypesApi)
+    assert hasattr(apis, "v4")
