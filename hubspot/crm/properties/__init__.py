2183036ab5002b56fd991a0e8d0d17f501895fb9
alexey <azheltkovskiy@hubspot.com>
2023-03-02 11:49:54 +0300

    Regeneration crm properties(codegen only)

--- hubspot/crm/properties/__init__.py
+++ hubspot/crm/properties/__init__.py
@@ -29,22 +29,20 @@ from hubspot.crm.properties.exceptions import ApiTypeError
 from hubspot.crm.properties.exceptions import ApiValueError
 from hubspot.crm.properties.exceptions import ApiKeyError
 from hubspot.crm.properties.exceptions import ApiException
-
 # import models into sdk package
 from hubspot.crm.properties.models.batch_input_property_create import BatchInputPropertyCreate
 from hubspot.crm.properties.models.batch_input_property_name import BatchInputPropertyName
 from hubspot.crm.properties.models.batch_read_input_property_name import BatchReadInputPropertyName
 from hubspot.crm.properties.models.batch_response_property import BatchResponseProperty
-from hubspot.crm.properties.models.collection_response_property import CollectionResponseProperty
-from hubspot.crm.properties.models.collection_response_property_group import CollectionResponsePropertyGroup
+from hubspot.crm.properties.models.batch_response_property_with_errors import BatchResponsePropertyWithErrors
+from hubspot.crm.properties.models.collection_response_property_group_no_paging import CollectionResponsePropertyGroupNoPaging
+from hubspot.crm.properties.models.collection_response_property_no_paging import CollectionResponsePropertyNoPaging
 from hubspot.crm.properties.models.error import Error
 from hubspot.crm.properties.models.error_category import ErrorCategory
 from hubspot.crm.properties.models.error_detail import ErrorDetail
 from hubspot.crm.properties.models.model_property import ModelProperty
-from hubspot.crm.properties.models.next_page import NextPage
 from hubspot.crm.properties.models.option import Option
 from hubspot.crm.properties.models.option_input import OptionInput
-from hubspot.crm.properties.models.paging import Paging
 from hubspot.crm.properties.models.property_create import PropertyCreate
 from hubspot.crm.properties.models.property_group import PropertyGroup
 from hubspot.crm.properties.models.property_group_create import PropertyGroupCreate
@@ -53,3 +51,4 @@ from hubspot.crm.properties.models.property_modification_metadata import Propert
 from hubspot.crm.properties.models.property_name import PropertyName
 from hubspot.crm.properties.models.property_update import PropertyUpdate
 from hubspot.crm.properties.models.standard_error import StandardError
+
