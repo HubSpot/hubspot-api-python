b468a0a8c53a3b2e294f528a8147012138877f09
alexey <azheltkovskiy@hubspot.com>
2023-06-08 15:37:36 +0300

    Update get_auth_url func with tests

--- hubspot/utils/oauth.py
+++ hubspot/utils/oauth.py
@@ -1,14 +1,29 @@
 import urllib
 
+AUTHORIZE_URL = "https://app.hubspot.com/oauth/authorize"
 
-def get_auth_url(client_id, redirect_uri, scopes: tuple, optional_scopes=(), state=""):
-    return "https://app.hubspot.com/oauth/authorize?" + urllib.parse.urlencode(
-        {
-            "client_id": client_id,
-            "redirect_uri": redirect_uri,
-            "scope": " ".join(scopes),
-            "optional_scope": " ".join(optional_scopes),
-            "state": state,
-        },
+
+def get_auth_url(
+    client_id: str,
+    redirect_uri: str,
+    scopes: tuple = None,
+    optional_scopes: tuple = None,
+):
+    query_params = {
+        "client_id": client_id,
+        "redirect_uri": redirect_uri,
+    }
+
+    if scopes:
+        query_params["scope"] = ' '.join(scopes)
+
+    if optional_scopes:
+        query_params["optional_scope"] = ' '.join(optional_scopes)
+
+    params = urllib.parse.urlencode(
+        query_params,
+        safe='',
         quote_via=urllib.parse.quote,
     )
+
+    return f"{AUTHORIZE_URL}?{params}"
