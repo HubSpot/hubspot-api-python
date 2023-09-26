6e961cf844a792dbfa8bfd602d2d6ba1e86db3e8
alexey <azheltkovskiy@hubspot.com>
2023-09-26 11:17:30 +0300

    add webhooks tests

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -8,3 +8,10 @@ def api_client():
     client = hubspot.Client.create(access_token=os.getenv("HUBSPOT_ACCESS_TOKEN"))
 
     return client
+
+
+@pytest.fixture
+def api_client_by_api_key():
+    client = hubspot.Client.create(api_key=os.getenv("HUBSPOT_DEVELOPER_API_KEY"))
+
+    return client
