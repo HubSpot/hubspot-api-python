5bc4180aaa963ca7c90f0bc7f0b9be51e75f13ee
alexey <azheltkovskiy@hubspot.com>
2023-12-18 11:55:24 +0300

    codegen only

---
+++ hubspot/crm/objects/taxes/api/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from hubspot.crm.objects.taxes.api.basic_api import BasicApi
+from hubspot.crm.objects.taxes.api.batch_api import BatchApi
+from hubspot.crm.objects.taxes.api.gdpr_api import GDPRApi
+from hubspot.crm.objects.taxes.api.public_object_api import PublicObjectApi
+from hubspot.crm.objects.taxes.api.search_api import SearchApi
