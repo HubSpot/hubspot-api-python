38f655be8484f03f1ec6f02c12e169a40c2e9da9
alexey <azheltkovskiy@hubspot.com>
2024-03-04 16:08:29 +0300

    Discoveries: Update for CrmObjects

--- tests/spec/crm/test_deals.py
+++ tests/spec/crm/test_deals.py
@@ -1,11 +1,12 @@
 from hubspot import HubSpot
-from hubspot.crm.deals import BasicApi, BatchApi, SearchApi, PublicObjectApi
+from hubspot.crm.deals import BasicApi, BatchApi, GDPRApi, SearchApi, PublicObjectApi
 
 
 def test_is_discoverable():
     apis = HubSpot().crm.deals
     assert isinstance(apis.basic_api, BasicApi)
     assert isinstance(apis.batch_api, BatchApi)
+    assert isinstance(apis.gdpr_api, GDPRApi)
     assert isinstance(apis.search_api, SearchApi)
     assert isinstance(apis.public_object_api, PublicObjectApi)
     assert hasattr(apis, "get_all")
