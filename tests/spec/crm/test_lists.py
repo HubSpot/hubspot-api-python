52c7009b4a9464fbb63fad12d8a9442c4cab2b38
alexey <azheltkovskiy@hubspot.com>
2024-03-05 13:09:46 +0300

    Discoveries: Update for Clients

--- tests/spec/crm/test_lists.py
+++ tests/spec/crm/test_lists.py
@@ -1,9 +1,9 @@
 from hubspot import HubSpot
-from hubspot.crm.lists import ListsApi, MembershipsApi
+from hubspot.crm.lists import ListAppApi, ListAppMembershipApi
 
 
 def test_is_discoverable():
     apis = HubSpot().crm.lists
-    assert isinstance(apis.lists_api, ListsApi)
-    assert isinstance(apis.memberships_api, MembershipsApi)
+    assert isinstance(apis.list_app_api, ListAppApi)
+    assert isinstance(apis.list_app_membership_api, ListAppMembershipApi)
 
