df1eac9ed7b724c26912d0e95c5471e1bf137cc0
alexey <azheltkovskiy@hubspot.com>
2024-03-12 15:48:25 +0300

    Discoveries: Update Files, Events

--- tests/spec/events/send/test_send.py
+++ tests/spec/events/send/test_send.py
@@ -1,7 +1,7 @@
 from hubspot import HubSpot
-from hubspot.events.send import BehavioralEventsTrackingApi
+from hubspot.events.send import CustomEventDataApi
 
 
 def test_is_discoverable():
     apis = HubSpot().events.send
-    assert isinstance(apis.behavioral_events_tracking_api, BehavioralEventsTrackingApi)
+    assert isinstance(apis.custom_event_data_api, CustomEventDataApi)
