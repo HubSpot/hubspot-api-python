b80738ca5b79c4826cd74ffca2d61bf273f13f5e
alexey <azheltkovskiy@hubspot.com>
2024-09-09 12:54:11 +0300

    Update discoveries

--- tests/spec/crm/test_line_items.py
+++ tests/spec/crm/test_line_items.py
@@ -1,11 +1,9 @@
 from hubspot import HubSpot
-from hubspot.crm.line_items import BatchApi, GDPRApi, BasicApi, SearchApi, PublicObjectApi
+from hubspot.crm.line_items import BatchApi, BasicApi, SearchApi
 
 
 def test_is_discoverable():
     apis = HubSpot().crm.line_items
     assert isinstance(apis.basic_api, BasicApi)
-    assert isinstance(apis.gdpr_api, GDPRApi)
     assert isinstance(apis.batch_api, BatchApi)
     assert isinstance(apis.search_api, SearchApi)
-    assert isinstance(apis.public_object_api, PublicObjectApi)
