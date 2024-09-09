7863b8b921e0623c9585711efa3a9f1457bd6d9d
alexey <azheltkovskiy@hubspot.com>
2024-09-09 17:32:29 +0300

    Update discoveries

--- tests/spec/events/test_events.py
+++ tests/spec/events/test_events.py
@@ -1,7 +1,8 @@
 from hubspot import HubSpot
-from hubspot.events import EventsApi
+from hubspot.events import DefaultApi, EventsApi
 
 
 def test_is_discoverable():
     apis = HubSpot().events
+    assert isinstance(apis.default_api, DefaultApi)
     assert isinstance(apis.events_api, EventsApi)
