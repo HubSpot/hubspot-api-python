fe7d210d734a88a21932de68475d38fa0de9aa19
alexey <azheltkovskiy@hubspot.com>
2024-10-08 11:28:08 +0300

    clean up

--- hubspot/discovery/crm/contacts/discovery.py
+++ hubspot/discovery/crm/contacts/discovery.py
@@ -19,7 +19,7 @@ class Discovery(DiscoveryBase):
     @property
     def merge_api(self) -> api_client.MergeApi:
         return self._configure_api_client(api_client, "MergeApi")
-    
+
     @property
     def search_api(self) -> api_client.SearchApi:
         return self._configure_api_client(api_client, "SearchApi")
