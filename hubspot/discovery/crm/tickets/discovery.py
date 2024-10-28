7a6e25cfdb62332c432366aa93df01c8f97bebd2
alexey <azheltkovskiy@hubspot.com>
2024-10-28 09:03:47 +0300

    Fix tickets discovery

--- hubspot/discovery/crm/tickets/discovery.py
+++ hubspot/discovery/crm/tickets/discovery.py
@@ -9,12 +9,16 @@ class Discovery(DiscoveryBase):
         return self._configure_api_client(api_client, "BasicApi")
 
     @property
-    def search_api(self) -> api_client.SearchApi:
-        return self._configure_api_client(api_client, "SearchApi")
-
-    @property
     def batch_api(self) -> api_client.BatchApi:
         return self._configure_api_client(api_client, "BatchApi")
+
+    @property
+    def merge_api(self) -> api_client.MergeApi:
+        return self._configure_api_client(api_client, "MergeApi")
+
+    @property
+    def search_api(self) -> api_client.SearchApi:
+        return self._configure_api_client(api_client, "SearchApi")
 
     def get_all(self, **kwargs):
         return fetch_all(self.basic_api, **kwargs)
