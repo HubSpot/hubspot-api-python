2c2a1a6df451b1350cdaf8234602387803b929e1
alexey <azheltkovskiy@hubspot.com>
2024-11-27 12:41:38 +0300

    update discovery tests

--- tests/spec/marketing/test_marketing_events.py
+++ tests/spec/marketing/test_marketing_events.py
@@ -2,6 +2,7 @@ from hubspot import HubSpot
 from hubspot.marketing.events import (
     AttendanceSubscriberStateChangesApi,
     BasicApi,
+    ListAssociationsApi,
     ParticipantStateApi,
     SettingsApi,
     SubscriberStateChangesApi
@@ -12,6 +13,7 @@ def test_is_discoverable():
     apis = HubSpot().marketing.events
     assert isinstance(apis.attendance_subscriber_state_changes_api, AttendanceSubscriberStateChangesApi)
     assert isinstance(apis.basic_api, BasicApi)
+    assert isinstance(apis.list_associations_api, ListAssociationsApi)
     assert isinstance(apis.participant_state_api, ParticipantStateApi)
     assert isinstance(apis.settings_api, SettingsApi)
     assert isinstance(apis.subscriber_state_changes_api, SubscriberStateChangesApi)
