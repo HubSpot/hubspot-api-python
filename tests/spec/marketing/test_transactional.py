0c946c6935bf6b54f10a55def6e3cf11513e8b08
alexey <azheltkovskiy@hubspot.com>
2025-04-14 14:31:34 +0300

    Discovery Marketing Transactional

--- tests/spec/marketing/test_transactional.py
+++ tests/spec/marketing/test_transactional.py
@@ -1,8 +1,8 @@
 from hubspot import HubSpot
-from hubspot.marketing.transactional import PublicSmtpTokensApi, SingleSendApi
+from hubspot.marketing.transactional import PublicSMTPTokensApi, SingleSendApi
 
 
 def test_is_discoverable():
-    apis = HubSpot().marketing.transactional 
-    assert isinstance(apis.public_smtp_tokens_api, PublicSmtpTokensApi)
+    apis = HubSpot().marketing.transactional
+    assert isinstance(apis.public_smtp_tokens_api, PublicSMTPTokensApi)
     assert isinstance(apis.single_send_api, SingleSendApi)
