411e0843701910c803d395910d60c4f1776df148
alexey <azheltkovskiy@hubspot.com>
2025-04-14 16:20:39 +0300

    Discovery Events

--- tests/spec/events/send/test_send.py
+++ tests/spec/events/send/test_send.py
@@ -1,7 +1,8 @@
 from hubspot import HubSpot
-from hubspot.events.send import CustomEventDataApi
+from hubspot.events.send import BasicApi, BatchApi
 
 
 def test_is_discoverable():
     apis = HubSpot().events.send
-    assert isinstance(apis.custom_event_data_api, CustomEventDataApi)
+    assert isinstance(apis.basic_api, BasicApi)
+    assert isinstance(apis.batch_api, BatchApi)
--- tests/spec/events/test_events.py
+++ tests/spec/events/test_events.py
@@ -1,8 +1,7 @@
 from hubspot import HubSpot
-from hubspot.events import DefaultApi, EventsApi
+from hubspot.events import EventsApi
 
 
 def test_is_discoverable():
     apis = HubSpot().events
-    assert isinstance(apis.default_api, DefaultApi)
     assert isinstance(apis.events_api, EventsApi)
