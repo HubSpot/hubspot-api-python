7de4d83a149d87fed5dfce41de0bc4c0e713d6dc
alexey <azheltkovskiy@hubspot.com>
2025-04-14 15:37:52 +0300

    Discovery Crm Extansions Calling

--- tests/spec/crm/extensions/test_calling.py
+++ tests/spec/crm/extensions/test_calling.py
@@ -1,8 +1,9 @@
 from hubspot import HubSpot
-from hubspot.crm.extensions.calling import SettingsApi, RecordingSettingsApi
+from hubspot.crm.extensions.calling import ChannelConnectionSettingsApi, SettingsApi, RecordingSettingsApi
 
 
 def test_is_discoverable():
     apis = HubSpot().crm.extensions.calling
+    assert isinstance(apis.channel_connection_settings_api, ChannelConnectionSettingsApi)
     assert isinstance(apis.settings_api, SettingsApi)
     assert isinstance(apis.recording_settings_api, RecordingSettingsApi)
