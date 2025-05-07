f4c41c37e0a52a1db711297b9cea39a3043d6279
alexey <azheltkovskiy@hubspot.com>
2025-05-07 14:05:27 +0300

    Discovery Crm Tickets

--- tests/spec/crm/test_tickets.py
+++ tests/spec/crm/test_tickets.py
@@ -6,6 +6,5 @@ def test_is_discoverable():
     apis = HubSpot().crm.tickets
     assert isinstance(apis.basic_api, BasicApi)
     assert isinstance(apis.batch_api, BatchApi)
-    assert isinstance(apis.merge_api, MergeApi)
     assert isinstance(apis.search_api, SearchApi)
     assert hasattr(apis, "get_all")
