edf4b93a95e5a9b389695dd157dae43463cb2362
alexey <azheltkovskiy@hubspot.com>
2025-05-07 12:08:31 +0300

    Codegen Crm Tickets

--- hubspot/crm/tickets/api/__init__.py
+++ hubspot/crm/tickets/api/__init__.py
@@ -5,5 +5,4 @@ from __future__ import absolute_import
 # import apis into api package
 from hubspot.crm.tickets.api.basic_api import BasicApi
 from hubspot.crm.tickets.api.batch_api import BatchApi
-from hubspot.crm.tickets.api.merge_api import MergeApi
 from hubspot.crm.tickets.api.search_api import SearchApi
