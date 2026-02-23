6009eb979b4876631bab587609f66f261c3e473f
alexey <azheltkovskiy@hubspot.com>
2026-02-23 12:47:38 +0300

    Codegen: Marketing Emails

--- hubspot/marketing/emails/api/__init__.py
+++ hubspot/marketing/emails/api/__init__.py
@@ -3,5 +3,5 @@ from __future__ import absolute_import
 # flake8: noqa
 
 # import apis into api package
-from hubspot.marketing.emails.api.marketing_emails_api import MarketingEmailsApi
+from hubspot.marketing.emails.api.emails_api import EmailsApi
 from hubspot.marketing.emails.api.statistics_api import StatisticsApi
